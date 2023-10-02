f2ee47101fba32f900dca9fbe0e8b6aa731e5afc
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-10-03 03:20:44 +0600

    delete selected course

--- src/pages/Dashboard/Student/SelectedClasses/SelectedClasses.jsx
+++ src/pages/Dashboard/Student/SelectedClasses/SelectedClasses.jsx
@@ -1,17 +1,58 @@
 import { Link } from "react-router-dom";
 import useCart from "../../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useEffect, useState } from "react";
 
 const SelectedClasses = () => {
-    const [allselectedClasses, refetch] = useCart()
+    const [allselectedClasses, refetch] = useCart();
+    const [instance] = useAxiosSecure();
+    const [disabled, setDisabled] = useState(true);
 
     const totalPrice = allselectedClasses.reduce((sum, item) => item.coursePrice + sum, 0);
     // console.log(totalPrice);
+    useEffect(() => {
+        if(allselectedClasses.length > 0){
+            setDisabled(false)
+        }
+        else{
+            setDisabled(true);
+        }
+
+    }, [allselectedClasses])
+    const handleDeleteItem = async(item) => {
+        event.preventDefault();
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                instance.delete(`/carts/${item._id}`)
+                .then(res => {
+                    // console.log(res?.data)
+                    if(res?.data?.deletedCount){
+                        refetch();
+                        Swal.fire(
+                            'Deleted!',
+                            'Your selected course has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+            }
+          })
+    }
 
     return (
         <div className="w-full p-4">
             <div className="flex justify-between items-center md:w-11/12">
                 <h2 className=" text-3xl font-semibold my-10">Total Price: ${totalPrice} </h2>
-                <Link to='/dashboard/payments' className="btn btn-sm bg-[#01a2a6] text-white">Pay</Link>
+                <Link to='/dashboard/payments' className="btn btn-sm bg-[#01a2a6] text-white" disabled = {disabled}>Pay</Link>
             </div>
             <div>
                 <div className="overflow-x-auto">
@@ -47,7 +88,7 @@ const SelectedClasses = () => {
                                 <div className="text-sm opacity-50">{item.instructorEmail}</div>
                                 </td>
                                 <td>
-                                <button className="btn btn-circle btn-outline">
+                                <button className="btn btn-circle btn-outline" onClick={() => handleDeleteItem(item)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                 </button>
                                 </td>
